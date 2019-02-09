4ba2de85525c9f240d2ff5a0286f9ed1eb4137bc
Aidan <aidanirrgang@gmail.com>
2019-02-09 17:00:05 +1100

    Initial commit.

---
+++ compare_html.py
@@ -0,0 +1,40 @@
+import sys
+from bs4 import BeautifulSoup
+
+
+def get_news(html_doc):
+
+    div = html_doc.find('div', id="news")
+    news_items = div.findChildren('h4', recursive=False)
+    return news_items
+
+
+def compare_news(page1, page2):
+
+    page1_bs = BeautifulSoup(page1, features="html5lib")
+    page2_bs = BeautifulSoup(page2, features="html5lib")
+
+    page1_news = get_news(page1_bs)
+    page2_news = get_news(page2_bs)
+
+    if page1_news[0].text == page2_news[0].text:
+        return 0
+    else:
+        return -1
+
+
+def main(args):
+
+    if len(args) != 3:
+        raise ValueError("Expected 2 arguments, got {}".format(len(args) - 1))
+
+    ret_code = compare_news(args[1], args[2])
+
+    print(ret_code)
+
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+
+    main(sys.argv)
